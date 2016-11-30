8328a76b831e831837d209e75f3cfb511857cb85
Kyoungrok Jang <kyoungrok.jang@gmail.com>
2016-11-30 13:37:30 +0900

    add docstring

--- fhee.py
+++ fhee.py
@@ -5,11 +5,17 @@ app = Flask(__name__)
 # /
 @app.route('/')
 def index():
+    """Dummy index page"""
     return 'Index Page'
 
 # /events
 @app.route('/events')
 def events():
+    """Returns a list of events.
+
+    Returns:
+        events (list): a list of events
+    """
     # get pagination params
     page = request.args.get('page', 1)
     limit = request.args.get('limit', 20)
@@ -20,6 +26,33 @@ def events():
 # /events/uid
 @app.route('/events/<int:uid>')
 def event(uid):
+    """Return a single event
+
+    Args:
+        uid (int): an event unique id
+
+    Returns:
+        event (:obj:`event`): an event object
+
+            {
+                "uid": "",
+                "datetime": "",
+                "media": "",
+                "text": {
+                    "title": "",
+                    "processed": ""
+                },
+                "keywords": {
+                    "food": "",
+                    "ing": "",
+                    "bio": "",
+                    "chem": "",
+                    "germ": "",
+                    "city": "",
+                    "county": ""
+                }
+            }
+    """
     # do DB query
     return '{}'.format(uid)
     
