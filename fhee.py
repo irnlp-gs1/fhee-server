bb11f1a953ff552e2d963232fa6a340226dd7df8
Kyoungrok Jang <kyoungrok.jang@gmail.com>
2016-11-30 19:04:43 +0900

    /event 구현

--- fhee.py
+++ fhee.py
@@ -1,6 +1,35 @@
 from flask import Flask
 from flask import request
+from flask import jsonify
+from pyArango.connection import Connection
 app = Flask(__name__)
+
+DB = 'gs1'
+COLLECTION = 'fhee'
+
+def _get_db():
+    """Return DB & collection
+    
+    Returns:
+        (db, collection)
+    """
+    conn = Connection(arangoURL='http://arango.kyoungrok.com',
+                      username='root',
+                      password='ir7753nlp!')
+
+    if not conn.hasDatabase(DB):
+        db = conn.createDatabase(DB)
+    else:
+        db = conn[DB]
+
+    if not db.hasCollection(COLLECTION):
+        collection = db.createCollection(name=COLLECTION)
+    else:
+        collection = db.collections[COLLECTION]
+
+    return db, collection
+
+db, collection = _get_db()
 
 # /
 @app.route('/')
@@ -32,7 +61,7 @@ def event(uid):
         uid (int): an event unique id
 
     Returns:
-        event (:obj:`event`): an event object
+        event (:obj:`Event`): an event object
 
             {
                 "uid": "",
@@ -43,18 +72,19 @@ def event(uid):
                     "processed": ""
                 },
                 "keywords": {
-                    "food": "",
-                    "ing": "",
-                    "bio": "",
-                    "chem": "",
-                    "germ": "",
-                    "city": "",
-                    "county": ""
+                    "food": [],
+                    "ing": [],
+                    "bio": [],
+                    "chem": [],
+                    "germ": [],
+                    "city": [],
+                    "county": []
                 }
             }
     """
     # do DB query
-    return '{}'.format(uid)
-    
+    doc = collection.fetchFirstExample(exampleDict={'uid': int(uid)})
+    return jsonify(doc)
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
